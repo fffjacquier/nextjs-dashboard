3bfab33a53041974d2572750958a51da41bb8f83
Francois Jacquier <fjacquier@gmail.com>
2023-11-13 17:01:44 +0100

    add login, logout, metadata

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,7 @@ import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import { z } from 'zod';
+import { signIn } from '@/app/auth';
 
 const InvoiceSchema = z.object({
   id: z.string(),
@@ -109,5 +110,25 @@ export async function deleteInvoice(id: string) {
     return { message: 'Deleted Invoice.' };
   } catch (error) {
     return { message: 'Database Error: Failed to Delete Invoice.' };
+  }
+}
+
+export async function authenticate(prevState: string | undefined, formData: FormData) {
+  let responseRedirectUrl = null;
+  try {
+    console.log('formData', formData);
+    responseRedirectUrl = await signIn('credentials', {
+      ...Object.fromEntries(formData),
+      redirect: false,
+    });
+  } catch (error) {
+    console.log('error', error);
+    if ((error as Error).message.includes('CredentialsSignin')) {
+      return 'CredentialSignin';
+    }
+    throw error;
+  } finally {
+    console.log(responseRedirectUrl);
+    if (responseRedirectUrl) redirect('/dashboard');
   }
 }
